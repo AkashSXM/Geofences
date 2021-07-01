8b4470a799016f9c023cf38025537b577cfec7da
aagarwalSirius <86786692+aagarwalSirius@users.noreply.github.com>
2021-07-01 12:56:08 -0400

    Fixed Bugs on GetStaticMap

--- Summer2021/data.py
+++ Summer2021/data.py
@@ -1,7 +1,8 @@
 import requests
 import json
-# import numpy as np
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
 GOOGLE_KEY = "AIzaSyCqo0_1Yl5HXfELsH50WtVEdH0yUOlJe40"
 
@@ -16,10 +17,22 @@ def place_by_id(placeId):
     return res
 
 def get_static_map(latitude, longditude, zoom, size, maptype):
-    req = requests.get("https://maps.googleapis.com/maps/api/staticmap?center=%f,%f&zoom=%f&size=%s&maptype=%s&key=%s" % (latitude, longditude, zoom, size, maptype, GOOGLE_KEY))
-    res = req.json()
-    return res
+    URL = "https://maps.googleapis.com/maps/api/staticmap?center=%f,%f&zoom=%i&size=%s&maptype=%s&format=jpg&key=%s" % (latitude, longditude, zoom, size, maptype, GOOGLE_KEY)
+    print(URL)
+    req = requests.get(URL)
+    # resp = req.raw
+    # image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return req.content
 
 if __name__ == "__main__":
     place = find_place("Gas", "textquery", 33.1502061, -96.8978388)
     Map = get_static_map(33.1502061, -96.8978388, 19, "600x300", "roadmap")
+    with open('newTest.png', 'wb') as file:
+        file.write(Map)
+
+    # https://maps.googleapis.com/maps/api/staticmap?center=33.150206,-96.897839&zoom=19.000000&size=600x300&maptype=roadmap&format=jpg&key=AIzaSyCqo0_1Yl5HXfELsH50WtVEdH0yUOlJe40
+    # https://maps.googleapis.com/maps/api/staticmap?center=33.1502061,-96.8978388&zoom=19&size=600x300&maptype=roadmap&key=AIzaSyCqo0_1Yl5HXfELsH50WtVEdH0yUOlJe40
+    # Map = requests.get("https://maps.googleapis.com/maps/api/staticmap?center=33.1502061,-96.8978388&zoom=19&size=600x300&maptype=roadmap&key=AIzaSyCqo0_1Yl5HXfELsH50WtVEdH0yUOlJe40")
+    # with open('mapTest.png', 'wb') as file:
+    #     file.write(Map.content)
