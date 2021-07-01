a92d93be5cbbe92a9d73a07b2386f675f41849d2
aagarwalSirius <86786692+aagarwalSirius@users.noreply.github.com>
2021-07-01 13:25:38 -0400

    getImages loop

--- Summer2021/data.py
+++ Summer2021/data.py
@@ -27,9 +27,26 @@ def get_static_map(latitude, longditude, zoom, size, maptype):
 
 if __name__ == "__main__":
     place = find_place("Gas", "textquery", 33.1502061, -96.8978388)
-    Map = get_static_map(33.1502061, -96.8978388, 19, "600x300", "roadmap")
-    with open('newTest.png', 'wb') as file:
-        file.write(Map)
+
+
+    # Read and parse coordinates
+    coords = np.genfromtxt("data/coords/100_coords.csv", delimiter=",")
+    print(coords[1:, 2:].shape)
+    coords = coords[1:, 2:]
+
+    # Loop thorugh each coord and get image
+    mode = "satellite"
+    zoom = 18
+    map_size = "600x600"
+    for coord in coords:
+        img = get_static_map(coord[0], coord[1], zoom, map_size, mode)
+        with open('data/images/%s/%f_%f_%i_%s.png' % (mode, coord[0], coord[1], zoom, map_size), 'wb') as file:
+            file.write(img)
+
+
+    # Map = get_static_map(33.1502061, -96.8978388, 19, "600x300", "roadmap")
+    # with open('data/%f_%f.png', 'wb') as file:
+    #     file.write(Map)
 
     # https://maps.googleapis.com/maps/api/staticmap?center=33.150206,-96.897839&zoom=19.000000&size=600x300&maptype=roadmap&format=jpg&key=AIzaSyCqo0_1Yl5HXfELsH50WtVEdH0yUOlJe40
     # https://maps.googleapis.com/maps/api/staticmap?center=33.1502061,-96.8978388&zoom=19&size=600x300&maptype=roadmap&key=AIzaSyCqo0_1Yl5HXfELsH50WtVEdH0yUOlJe40
